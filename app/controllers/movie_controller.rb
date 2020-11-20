738fa1d560fdd95302a9f3b1785acda5f2e32ba5
Scot <rss3509@gmail.com>
2020-11-20 10:20:25 -0800

    env added

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,8 +1,11 @@
 class MovieController < ApplicationController
 
+    API_KEY = ENV['api_key']
+
     def show
-        response = RestClient.get 'https://api.themoviedb.org/3/movie/top_rated?api_key=7c52f138f088edd2d7ac5f6212bc199e'
-        json = JSON.parse response
+        response = RestClient.get 'https://api.themoviedb.org/3/movie/top_rated?api_key=#{API_KEY}'
+        json = JSON.parse(response)
+        render json: json 
         # byebug
     end
 end
